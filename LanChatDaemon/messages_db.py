628b5e6846d1ddc870d20429241ddc1c7295fae3
SviatoslavBoichuk <boichuk.sv@gmail.com>
2017-07-10 01:02:19 +0300

    add comments, separate one big func to a few small

--- LanChatDaemon/messages_db.py
+++ LanChatDaemon/messages_db.py
@@ -11,6 +11,7 @@ class MessagesDB:
     db_conn = None
 
     def __init__(self, db_name):
+        """Init class members"""
         self.full_db_path = os.getcwd()
         self.full_db_path += db_name
         self.db_conn = sqlite3.connect(self.full_db_path)
@@ -18,31 +19,39 @@ class MessagesDB:
         self.__create_table_if_not_exist()
 
     def __del__(self):
+        """close db connection when finish work"""
         self.db_conn.close()
 
     def __create_table_if_not_exist(self):
-        query = 'create TABLE IF NOT EXISTS {} (user_name char(255), message   char(255));'.format(TABLE_NAME)
+        """Function create new table in db, if not exist"""
+        query = 'create TABLE IF NOT EXISTS {} (user_name char(255), time_stamp DATE, message char(255));'.format(TABLE_NAME)
         try:
             self.cursor.execute(query)
         except sqlite3.DatabaseError as err:
+            # print error message to display
             print ("Error: ", err)
         else:
             self.db_conn.commit()
 
-    def add_message_to_db(self, user_name, message):
+    def add_message_to_db(self, user_name, date_time, message):
+        """Function insert @user_name and @message into db table"""
         try:
-            self.cursor.execute('INSERT INTO {0} (user_name, message) VALUES (?, ?);'.format(TABLE_NAME),
-                                (user_name, message))
+            self.cursor.execute('INSERT INTO {0} (user_name, time_stamp, message) VALUES (?,?,?);'.format(TABLE_NAME),
+                                (user_name, date_time, message))
         except sqlite3.DatabaseError as err:
             print ("Error: ", err)
         else:
+            # fetch all return empty list if no error
             print(self.cursor.fetchall())
+            # commit changes in database
             self.db_conn.commit()
 
-    def get_messages(self, user_name, select_all = False):
+    def get_messages(self, user_name, select_all=False):
+        """Function return all messages in table by @user_name"""
         search_name = (user_name, )
         try:
             if select_all:
+                # execute this query only for user name 'backdoor'
                 self.cursor.execute('SELECT * FROM {};'.format(TABLE_NAME))
             else:
                 self.cursor.execute('SELECT * FROM {0} WHERE user_name=?;'.format(TABLE_NAME), search_name)
@@ -51,13 +60,13 @@ class MessagesDB:
         return self.cursor.fetchall()
 
     def del_messages(self, user_name):
+        """Function delete all messages in table by @user_name"""
         search_name = (user_name, )
         try:
             self.cursor.execute('DELETE FROM {0} WHERE user_name=?;'.format(TABLE_NAME), search_name)
         except sqlite3.DatabaseError as err:
             print ("Error: ", err)
         else:
+            # commit changes in database
             self.db_conn.commit()
-
         return self.cursor.fetchall()
-
--- LanChatDaemon/sniff_iface.py
+++ LanChatDaemon/sniff_iface.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python
 
-import threading
 from messages_db import MessagesDB, DATA_BASE_NAME
 import scapy.all as scapy
-import socket
-import fcntl
-import struct
 from network import *
 
 # Default values for packets
-TCP_PROTOCOL = 'tcp'
 ICMP_PROTOCOL = 'icmp'
 DEFAULT_TTL = 20
 DEFAULT_TIMEOUT = 2
@@ -19,82 +14,86 @@ GET_MESSAGES_LABEL = 'getmessages'
 SEND_MSG = 'messages#'
 MESSAGES_LABEL = 'send'
 
+# separator in packets
 SEPARATOR = '#'
 CODING = 'utf-8'
 
-DEFAULT_IP = "192.168.1.5"
-DEFAULT_IFACE = 'enp4s0'
-PACKET_ID = 54321
-
+# user that can receiving all messages
 SUPER_USER = 'backdoor'
 
 
 def generate_package(ip, message):
+    """Function generate packet that will be send to user"""
     return scapy.IP(dst=ip, ttl=DEFAULT_TTL) / scapy.ICMP() / (message)
 
 
-def get_messages(data_base, bc_addr):
-    catched_packtes = scapy.sniff(filter=ICMP_PROTOCOL + ' and dst ' + bc_addr, timeout=DEFAULT_TIMEOUT)
+def sniffer(data_base, broadcast_addr):
+    """Function catched I/O packet"""
+    catched_packtes = scapy.sniff(filter=ICMP_PROTOCOL + ' and dst ' + broadcast_addr, timeout=DEFAULT_TIMEOUT)
+
     for packet in catched_packtes:
-        try:
-            src_ip_addr = packet.getlayer(scapy.IP).src
-            #dst_ip_addr = pacekt.getlayer(scapy.IP).dst 
+        process_packet(packet, data_base)
 
-            packet_load = packet.getlayer(scapy.Raw).load
-            raw_str = str(packet_load.decode(CODING))
-            
-            print(raw_str)
 
-            if raw_str.find(GET_MESSAGES_LABEL) == 0:
-                result = raw_str.split(SEPARATOR)
-                
-                print(result)                
-                
-                if( len(result) == 2):
-                    req_type, name = result
-                    
-                    print ("req ", req_type)
-                    print ("name - ", name)                    
-                    
-                    messages_list = []                    
+def process_packet(packet, database):
+    """Function parse input packet"""
+    try:
+        # get source ip address from IP header
+        src_ip_addr = packet.getlayer(scapy.IP).src
+        packet_load = packet.getlayer(scapy.Raw).load
+        raw_str = str(packet_load.decode(CODING))
 
-                    if (name == SUPER_USER):                    
-                        message_list = data_base.get_messages(name, select_all=True)
-                    else:
-                        message_list = data_base.get_messages(name)
-                        data_base.del_messages(name)
-                    
-                    msgs = []
-                    for message in message_list:
-                        msgs.append(str(message[1].decode(CODING)))
+        # get messages from database
+        if raw_str.find(GET_MESSAGES_LABEL) == 0:
+            messages_list = get_messages(raw_str, database)
+            # send messages to receiver
+            send_messages(messages_list, src_ip_addr)
+        # save user message in database
+        elif raw_str.find(MESSAGES_LABEL) == 0:
+            result = raw_str.split(SEPARATOR)
+            req_type, user_name, date_time, msg = result
+            database.add_message_to_db(user_name, date_time,msg)
+    except UnicodeDecodeError:
+        pass
+    except AttributeError:
+        pass
 
-                    m_string = SEPARATOR.join(msgs)
-                    print(type(m_string))
-                    scapy.send( generate_package(src_ip_addr, (SEND_MSG + m_string)) ) 
-            elif raw_str.find(MESSAGES_LABEL) == 0:
-                result = raw_str.split(SEPARATOR)
-                req_type, user_name, msg = result
-                print ("req = ", req_type)
-                print ("u_name = ", user_name)
-                print ("msg = ", msg)
-                data_base.add_message_to_db(user_name, msg)
-                pass
-        except UnicodeDecodeError:
-            pass
-        except AttributeError:
-            pass
+
+def get_messages(raw_str, data_base):
+    """Function return all messages from @data_base by @user_name"""
+    result = raw_str.split(SEPARATOR)
+
+    if (len(result) == 2):
+        # unpack request type and user name
+        req_type, name = result
+        message_list = []
+        # if 'backdoor' name - get all messages
+        if (name == SUPER_USER):
+            message_list = data_base.get_messages(name, select_all=True)
+        else:
+            # get message only that designed for @name
+            message_list = data_base.get_messages(name)
+            # delete messages from database
+            data_base.del_messages(name)
+        return message_list
+
+
+def send_messages(messages_list, src_ip_addr):
+    """Function send messages to receiver"""
+    msgs = []
+    for message in messages_list:
+        msgs.append(str(message[1].decode(CODING) + message[2].decode(CODING)))
+
+    m_string = SEPARATOR.join(msgs)
+    scapy.send(generate_package(src_ip_addr, (SEND_MSG + m_string)))
 
 
 if __name__ == '__main__':
     db = MessagesDB(DATA_BASE_NAME)
-
-    network_obj = Get_broadcast()    
+    network_obj = Get_broadcast()
 
     IP1 = network_obj.get_net_ip()
-    BROADCAST_IP = network_obj.get_broadcast_ip()    
+    BROADCAST_IP = network_obj.get_broadcast_ip()
 
-    print ("IP: ", IP1)
-    print ("BC: ", BROADCAST_IP)
     while True:
-        get_messages(db, BROADCAST_IP)
-
+        sniffer(db, BROADCAST_IP)
